134dc289ff3992ddeac589f86b40e6c3ad64801e
Jos <118079175+PanditaGeek@users.noreply.github.com>
2025-05-28 22:27:43 -0500

    Lab2.cpp

--- Problema1.cpp
+++ Problema1.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void Fretiro(int arr[], int cant, int retiro ) {
-	int* sol = new int[cant];
-	if (retiro <= 0) return;
-	for (int i = cant-1; i >= 0; i--) {
-		if (retiro - arr[i] >= 0) {
-			retiro -= arr[i];
-			sol[i] = arr[i];
+void Fretiro(int arr[], int cant) {
+	int suma = 0;
+	int contador = 0;
+
+	cout << "Monedas seleccionadas: ";
+	
+	for (int i = 0; i < cant - 1; i++) {
+		if (suma + arr[i] < arr[i + 1]) {
+			cout << arr[i] << " ";
+			suma += arr[i];
+			contador++;
 		}
-		else
-			sol[i] = 0;
 	}
-	for (int i = 0; i < cant; i++)
-		if (sol[i] == 0) cout << "";
-		else
-			cout << sol[i] << " ";
-	delete[] sol;
+	
+	cout << arr[cant - 1] << endl; 
+	contador++;
+	
+	cout << "Total monedas diferentes: " << contador << endl;
 }
-int main()
-{
-	int c=0;
+
+int main() {
+	int c = 0;
 	cout << "Cantidad de monedas: " << endl;
 	cin >> c;
+
 	int* mon = new int[c];
-	cout << "Ingresar monedas: " << endl;
+	cout << "Ingresar monedas ordenadas por valor creciente: " << endl;
 	for (int i = 0; i < c; i++) {
 		cin >> mon[i];
 	}
-	int r = 0;
-	cout << "Ingresar retiro: " << endl;
-	cin >> r;
-	Fretiro(mon, c, r);
+
+	Fretiro(mon, c);
+	
 	delete[] mon;
 	return 0;
 }
+
